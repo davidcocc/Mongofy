4fc381f6b3c58d8d807b66c628444a214f8cf7dd
DJHeisenberg01 <91367092+DJHeisenberg01@users.noreply.github.com>
2024-05-23 16:31:45 +0200

    flask

---
+++ flaskTest.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return render_template(index.html)
