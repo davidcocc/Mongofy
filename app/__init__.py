fbfc059d4434ae482792df0215c958bf81f905dc
davidcocc <davidcoccorullo7@gmail.com>
2024-05-23 21:31:01 +0200

    fix?

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,9 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
+
+    app.static_folder = 'templates/static'
+
     from .routes import app_bp
     app.register_blueprint(app_bp)
 
