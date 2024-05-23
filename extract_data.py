a24a8f5c920d103c8e92a9ba7d818cedf3299379
davidcocc <davidcoccorullo7@gmail.com>
2024-05-23 19:11:21 +0200

    pulla!

--- extract_data.py
+++ extract_data.py
@@ -1,9 +1,6 @@
 import pandas as pd
 
 df = pd.read_csv("normalizedMusicData.csv")
-
-
-
 columns_to_drop = ["Playlist", "key", "mode", "liveness", "index", "Unnamed: 0", "acousticness", "loudness", "time_signature", "analysis_url", "track_href", "tempo", "uri"]
 df = df.drop(columns=columns_to_drop)
 col_id = df.pop('id')
